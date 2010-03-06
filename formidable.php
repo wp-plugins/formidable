6f8e755fb5bb99a1c2b228344ac1aa7b4423ff9d
sswells <sswells@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-06 18:49:25 +0000

    version num
    git-svn-id: https://plugins.svn.wordpress.org/formidable/trunk@214231 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- formidable.php
+++ formidable.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Formidable
 Description: Quickly and easily create drag-and-drop forms
-Version: 1.01.04
+Version: 1.02.0
 Plugin URI: http://blog.strategy11.com/formidable-wordpress-plugin
 Author URI: http://blog.strategy11.com
 Author: Stephanie Wells
