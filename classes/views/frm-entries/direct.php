86c4b1fe9614cb05c42a3d565ce227867c032ded
sswells <sswells@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-07 23:17:06 +0000

    Formidable 1.4.6 release
    git-svn-id: https://plugins.svn.wordpress.org/formidable/tags/1.04.06@356982 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/views/frm-entries/direct.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+
+<head>
+    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head() ?>
+</head>
+<body>
+    <?php require_once('frm-entry.php'); ?>
+    <?php wp_footer(); ?>
+</body>
+</html>
