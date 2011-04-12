2ec389cd3914ae897bd315dd57b80ea94be1eb89
sswells <sswells@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-12 23:36:43 +0000

    fix naming
    git-svn-id: https://plugins.svn.wordpress.org/formidable/tags/1.05.0@372217 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/views/frm-entries/direct.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head() ?>
+</head>
+<body>
+    <?php require_once('frm-entry.php'); ?>
+    <?php wp_footer(); ?>
+</body>
+</html>
---
+++ classes/views/frm-entries/list.php
@@ -0,0 +1,12 @@
+<div class="wrap">
+    <div id="icon-edit-pages" class="icon32"><br/></div>
+    <h2><?php _e('Pro Form Entries', 'formidable') ?></h2>
+
+    <?php require(FRM_VIEWS_PATH.'/shared/errors.php'); ?>
+
+    <?php require(FRM_VIEWS_PATH.'/shared/nav.php'); ?>
+
+    <p><?php printf(__('Upgrade to %1$sFormidable Pro%2$s to view, search, export, and bulk delete your saved entries.', 'formidable'), '<a href="http://formidablepro.com">', '</a>'); ?></p>
+
+</div>
+
