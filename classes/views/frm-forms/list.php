5dc8e79dcbfda42eccef2e01f0ef29836a657a31
sswells <sswells@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-06 19:49:07 +0000

    1.5.3 release
    git-svn-id: https://plugins.svn.wordpress.org/formidable/tags/1.05.03@393955 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/views/frm-forms/list.php
@@ -0,0 +1,152 @@
+<div class="wrap">
+    <div class="frmicon icon32"><br/></div>
+    <h2><?php echo ($params['template'])? __('Form Templates', 'formidable') : __('Forms', 'formidable'); 
+        if(!$params['template'] and current_user_can('frm_edit_forms')){ ?>
+        <a href="?page=<?php echo FRM_PLUGIN_NAME ?>-new" class="button add-new-h2"><?php _e('Add New', 'formidable'); ?></a>
+        <?php } ?>
+    </h2>
+  
+    <?php require(FRM_VIEWS_PATH.'/shared/errors.php'); ?>
+  
+    <?php do_action('frm_before_item_nav',$sort_str, $sdir_str, $search_str, false); ?>
+    <?php require(FRM_VIEWS_PATH.'/shared/nav.php'); ?>
+
+<?php if ($params['template']) require('default-templates.php'); ?>
+        
+<form class="form-fields item-list-form" name="item_list_form" id="posts-filter" method="post" >
+  <input type="hidden" name="action" value="list-form"/>
+  <input type="hidden" name="template" value="<?php echo $params['template'] ?>" />   
+<?php $footer = false; require(FRM_VIEWS_PATH.'/shared/item-table-nav.php'); ?>
+<table class="widefat fixed" cellspacing="0">
+    <thead>
+    <tr>
+        <th class="manage-column check-column" scope="col"> <?php do_action('frm_column_header'); ?> </th>
+        <?php if ($params['template']){ ?>
+            <th class="manage-column" width="">
+                  <a href="?page=<?php echo FRM_PLUGIN_NAME; ?>-templates&amp;sort=name&amp;sdir=<?php echo (($sort_str == 'name' and $sdir_str == 'desc')?'asc':'desc'); ?>"><?php _e('Name', 'formidable') ?><?php echo (($sort_str == 'name')?' &nbsp; <img src="'.FRM_URL.'/images/'.(($sdir_str == 'desc')?'arrow_down.png':'arrow_up.png').'"/>':'') ?></a>
+            </th>
+            <th class="manage-column">
+                <a href="?page=<?php echo FRM_PLUGIN_NAME; ?>-templates&amp;sort=description&amp;sdir=<?php echo (($sort_str == 'description' and $sdir_str == 'desc')?'asc':'desc'); ?>"><?php _e('Description', 'formidable') ?><?php echo (($sort_str == 'description')?' &nbsp; <img src="'.FRM_URL.'/images/'.(($sdir_str == 'desc')?'arrow_down.png':'arrow_up.png').'"/>':'') ?></a>
+            </th>
+        <?php }else{?>
+            <th class="manage-column" width="50px">
+                <a href="?page=<?php echo FRM_PLUGIN_NAME; ?>&amp;sort=id&amp;sdir=<?php echo (($sort_str == 'id' and $sdir_str == 'desc')?'asc':'desc'); ?>"><?php _e('ID', 'formidable') ?><?php echo (($sort_str == 'id')?' &nbsp; <img src="'.FRM_URL.'/images/'.(($sdir_str == 'desc')?'arrow_down.png':'arrow_up.png').'"/>':'') ?></a>
+            </th>
+            <th class="manage-column" width="">
+                <a href="?page=<?php echo FRM_PLUGIN_NAME; ?>&amp;sort=name&amp;sdir=<?php echo (($sort_str == 'name' and $sdir_str == 'desc')?'asc':'desc'); ?>"><?php _e('Name', 'formidable') ?><?php echo (($sort_str == 'name')?' &nbsp; <img src="'.FRM_URL.'/images/'.(($sdir_str == 'desc')?'arrow_down.png':'arrow_up.png').'"/>':'') ?></a>
+            </th>
+            <th class="manage-column">
+                <a href="?page=<?php echo FRM_PLUGIN_NAME; ?>&amp;sort=description&amp;sdir=<?php echo (($sort_str == 'description' and $sdir_str == 'desc')?'asc':'desc'); ?>"><?php _e('Description', 'formidable') ?><?php echo (($sort_str == 'description')?' &nbsp; <img src="'.FRM_URL.'/images/'.(($sdir_str == 'desc')?'arrow_down.png':'arrow_up.png').'"/>':'') ?></a>
+            </th>
+            <th class="manage-column" width="70px">
+                <a href="?page=<?php echo FRM_PLUGIN_NAME; ?>&amp;sort=form_key&amp;sdir=<?php echo (($sort_str == 'form_key' and $sdir_str == 'desc')?'asc':'desc'); ?>"><?php _e('Key', 'formidable') ?><?php echo (($sort_str == 'form_key')?' &nbsp; <img src="'.FRM_URL.'/images/'.(($sdir_str == 'desc')?'arrow_down.png':'arrow_up.png').'"/>':'') ?></a>
+            </th>
+            <th class="manage-column" width="60px"><?php _e('Entries', 'formidable') ?></th>
+            <th class="manage-column" width="115px"><?php _e('Direct Link', 'formidable') ?></th>
+            <th class="manage-column" width="115px"><?php _e('ShortCodes', 'formidable') ?></th>
+        <?php } ?>
+    </tr>
+    </thead>
+    <tbody>
+<?php if($record_count <= 0){ ?>
+    <tr>
+      <td colspan="<?php echo ($params['template'])? '2':'7'; ?>"><?php _e('No Forms Found', 'formidable') ?></td>
+    </tr>
+<?php
+}else{
+    $alternate = '';
+    foreach($forms as $form){
+        $alternate = (empty($alternate)) ? ' alternate' : '';
+?>
+    <tr class="iedit<?php echo $alternate ?>" >
+        <th class="check-column" scope="row"><?php do_action('frm_first_col', $form->id); ?></th>
+        <?php if ($params['template']){ ?>
+        <td class="post-title">
+            <?php if(current_user_can('frm_edit_forms')){ ?>
+            <a class="row-title" href="?page=<?php echo FRM_PLUGIN_NAME; ?>&amp;action=edit&amp;id=<?php echo $form->id; ?>" title="<?php _e('Edit', 'formidable') ?> <?php echo stripslashes($form->name); ?>"><?php echo stripslashes($form->name); ?></a>
+            <?php }else{    
+                    echo stripslashes($form->name); 
+                  } ?>
+            <br/>
+            <div class="row-actions">
+                <?php if(current_user_can('frm_edit_forms')){ ?>
+                <span><a href="?page=<?php echo FRM_PLUGIN_NAME; ?>&amp;action=duplicate&amp;id=<?php echo $form->id; ?>" title="<?php _e('Copy', 'formidable') ?> <?php echo $form->name; ?>"><?php _e('Create Form from Template', 'formidable') ?></a></span>
+                | <span class="edit"><a href="?page=<?php echo FRM_PLUGIN_NAME; ?>&amp;action=edit&amp;id=<?php echo $form->id; ?>" title="<?php _e('Edit', 'formidable') ?> <?php echo $form->name; ?>"><?php _e('Edit', 'formidable') ?></a></span>
+                <?php } ?>
+                <?php do_action('frm_template_action_links', $form); ?>
+                <?php if(current_user_can('frm_delete_forms')){ ?>
+                | <span class="trash"><a href="?page=<?php echo FRM_PLUGIN_NAME; ?>&amp;action=destroy&amp;id=<?php echo $form->id; ?>"  onclick="return confirm('<?php printf(__('Are you sure you want to delete your %1$s Form?', 'formidable'), stripslashes($form->name)) ?>');" title="<?php _e('Delete', 'formidable') ?> <?php echo $form->form_key; ?>"><?php _e('Delete', 'formidable') ?></a></span>
+                <?php } ?>
+            </div>
+        </td>
+        <td><?php echo $form->description ?></td>
+        <?php }else{ ?>
+        <td><?php echo $form->id ?></td>
+        <td class="post-title">
+            <?php if(current_user_can('frm_edit_forms')){ ?>
+            <a class="row-title" href="?page=<?php echo FRM_PLUGIN_NAME; ?>&amp;action=edit&amp;id=<?php echo $form->id; ?>" title="<?php _e('Edit', 'formidable') ?> <?php echo stripslashes($form->name); ?>"><?php echo stripslashes($form->name); ?></a>
+            <?php }else{ ?>
+            <?php echo stripslashes($form->name); ?>
+            <?php }?>
+            <br/>
+            <div class="row-actions">
+                <?php if(current_user_can('frm_edit_forms')){ ?>
+                <span class="edit"><a href="?page=<?php echo FRM_PLUGIN_NAME; ?>&amp;action=edit&amp;id=<?php echo $form->id; ?>" title="<?php _e('Edit', 'formidable') ?> <?php echo htmlentities(stripslashes($form->name)) ?>"><?php _e('Edit', 'formidable') ?></a></span>
+                <?php } ?>
+                <?php if($frmpro_is_installed){ ?>
+                    <?php if(current_user_can('frm_create_entries')){ ?>
+                    | <span><a href="?page=<?php echo FRM_PLUGIN_NAME; ?>-entries&amp;action=new&amp;form=<?php echo $form->id; ?>" title="<?php _e('New', 'formidable') ?> <?php echo htmlentities(stripslashes($form->name)) ?> <?php _e('Entry', 'formidable') ?>"><?php _e('New Entry', 'formidable') ?></a></span>
+                    <?php } ?>
+                    <?php if(current_user_can('frm_view_entries')){ ?>
+                    | <span><a href="?page=<?php echo FRM_PLUGIN_NAME; ?>-entries&amp;form=<?php echo $form->id; ?>" title="<?php echo htmlentities(stripslashes($form->name)); ?> Entries"><?php _e('Entries', 'formidable') ?></a></span>
+                    <?php } ?>
+                    | <span><a href="?page=<?php echo FRM_PLUGIN_NAME; ?>-reports&amp;form=<?php echo $form->id; ?>" title="<?php echo htmlentities(stripslashes($form->name)); ?> Reports"><?php _e('Reports', 'formidable') ?></a></span>
+                <?php } ?>
+                <?php if(current_user_can('frm_edit_forms')){ ?>
+                | <span><a href="?page=<?php echo FRM_PLUGIN_NAME; ?>&amp;action=duplicate&amp;id=<?php echo $form->id; ?>" title="<?php _e('Copy', 'formidable') ?> <?php echo htmlentities(stripslashes($form->name)) ?>"><?php _e('Duplicate', 'formidable') ?></a></span>
+                | <span><a href="?page=<?php echo FRM_PLUGIN_NAME; ?>&amp;action=duplicate&amp;id=<?php echo $form->id; ?>&amp;template=1" title="<?php _e('Create', 'formidable') ?> <?php echo htmlentities(stripslashes($form->name)) ?> <?php _e('Template', 'formidable') ?>"><?php _e('Create Template', 'formidable') ?></a></span>
+                | <span><a href="<?php echo FRM_SCRIPT_URL ?>&amp;controller=forms&amp;action=export&amp;id=<?php echo $form->id; ?>" title="<?php _e('Export', 'formidable') ?> <?php echo htmlentities(stripslashes($form->name)) ?> <?php _e('Template', 'formidable') ?>"><?php _e('Export Template', 'formidable') ?></a></span>
+                <?php } ?>
+                <?php if(current_user_can('frm_delete_forms')){ ?>
+                | <span class="trash"><a href="?page=<?php echo FRM_PLUGIN_NAME; ?>&amp;action=destroy&amp;id=<?php echo $form->id; ?>"  onclick="return confirm('<?php printf(__('Are you sure you want to delete your %1$s Form?', 'formidable'), htmlentities(stripslashes($form->name))) ?>');" title="<?php _e('Delete', 'formidable') ?> <?php echo htmlentities(stripslashes($form->name)) ?>"><?php _e('Delete', 'formidable') ?></a></span>
+                <?php } ?>
+            </div>
+        </td>
+        <td><?php echo stripslashes($form->description) ?></td>
+        <td><?php echo $form->form_key ?></td>
+        <td><?php echo apply_filters('frm_view_entries_link', $frm_entry->getRecordCount("it.form_id=$form->id") . ' '. __('Entries', 'formidable'), $form->id); ?></td>
+        <td>
+            <input type='text' style="font-size: 10px; width: 100%;" readonly="true" onclick='this.select();' onfocus='this.select();' value='<?php echo $target_url = FrmFormsHelper::get_direct_link($form->form_key, $form->prli_link_id); ?>' /><br/><a href="<?php echo $target_url; ?>" target="blank"><?php _e('View Form', 'formidable') ?></a>
+        </td>
+        <td><input type='text' style="font-size:10px;width:100%;" readonly="true" onclick='this.select();' onfocus='this.select();' value='[formidable id=<?php echo $form->id; ?>]' /><br/>
+            <input type='text' style="font-size:10px;width:100%;" readonly="true" onclick='this.select();' onfocus='this.select();' value='[formidable key=<?php echo $form->form_key ?>]' />
+        </td>
+        <?php } ?>
+    </tr>
+      <?php
+    }
+  }
+?>
+    </tbody>
+    <tfoot>
+    <tr>
+        <th class="manage-column check-column" scope="col"> <?php do_action('frm_column_header'); ?> </th>
+        <?php if ($params['template']){ ?>
+            <th class="manage-column"><?php _e('Name', 'formidable') ?></th>
+            <th class="manage-column"><?php _e('Description', 'formidable') ?></th>
+        <?php }else{ ?>
+            <th class="manage-column"><?php _e('ID', 'formidable') ?></th>
+            <th class="manage-column"><?php _e('Name', 'formidable') ?></th>
+            <th class="manage-column"><?php _e('Description', 'formidable') ?></th>
+            <th class="manage-column"><?php _e('Key', 'formidable') ?></th>
+            <th class="manage-column"><?php _e('Entries', 'formidable') ?></th>
+            <th class="manage-column"><?php _e('Direct Link', 'formidable') ?></th>
+            <th class="manage-column"><?php _e('ShortCodes', 'formidable') ?></th>
+        <?php } ?>
+    </tr>
+    </tfoot>
+</table>
+<?php $footer = true; require(FRM_VIEWS_PATH.'/shared/item-table-nav.php'); ?>
+</form>
+
+</div>
---
+++ js/formidable_admin.js
@@ -0,0 +1,283 @@
+jQuery(document).ready(function($){
+$(".frm_elastic_text").elastic();
+	
+window.onscroll = document.documentElement.onscroll = frmSetMenuOffset;
+frmSetMenuOffset();
+
+if ($("input[name='options[success_action]']:checked").val() == 'redirect')
+    $('.success_action_redirect_box.success_action_box').fadeIn('slow');
+else if ($("input[name='options[success_action]']:checked").val() == 'page')
+	$('.success_action_page_box.success_action_box').fadeIn('slow');
+else
+	$('.success_action_message_box.success_action_box').fadeIn('slow');
+              
+$("input[name='options[success_action]']").change(function(){
+	$('.success_action_box').hide();
+    if($(this).val()=='redirect')
+        $('.success_action_redirect_box.success_action_box').fadeIn('slow');
+    else if($(this).val()=='page')
+        $('.success_action_page_box.success_action_box').fadeIn('slow');
+	else
+		$('.success_action_message_box.success_action_box').fadeIn('slow');
+});
+
+jQuery('.item-list-form').submit(function(){
+if(jQuery('#bulkaction').val()=='delete'){return confirm('Are you sure you want to delete each of the selected items below?');}
+});
+
+jQuery('.frm_single_option').hover(
+function(){jQuery(this).children(".frm_single_show_hover").show(); jQuery(this).children(".frm_spacer").hide();},
+function(){jQuery(this).children(".frm_single_show_hover").hide(); jQuery(this).children(".frm_spacer").show();}
+);
+
+jQuery('li.ui-state-default').click(function(evt){
+	var target = evt.target;
+	$('.frm-show-hover').hide(); $(this).children(".frm-show-hover").show();
+	$('.frm-show-click').hide(); $(this).children(".frm-show-click").show();
+	$('li.ui-state-default.selected').removeClass('selected'); $(this).addClass('selected');
+	if(!$(target).is('.inplace_field') && !$(target).is('.frm_ipe_field_label') && !$(target).is('.frm_ipe_field_desc') && !$(target).is('.frm_ipe_field_option')){ $('.inplace_field').blur();}
+});
+$("img.frm_help[title]").tooltip({tip:'#frm_tooltip',lazy:true});
+$("img.frm_help_text[title]").tooltip({tip:'#frm_tooltip_text',lazy:true});
+$("img.frm_help_big[title]").tooltip({tip:'#frm_tooltip_big',lazy:true});
+
+jQuery('.field_type_list > li').draggable({connectToSortable:'#new_fields',cursor:'move',helper:'clone',revert:'invalid',delay:10});
+jQuery("ul.field_type_list, .field_type_list li").disableSelection();
+
+/*
+jQuery('.frm-grid').find('input').live('keydown',function(e){
+	if(e.which >= 37 && e.which <= 40){
+		var matches;
+		var pattern= /_([0-9]+)_([0-9]+)$/;
+		if (matches=this.id.match(pattern)){
+			var row=parseInt(matches[1]); 
+			var col=parseInt(matches[2]);
+			var table=jQuery('#'+this.id).parents('.frm-grid');
+			var max_row=jQuery('#'+this.id).parents('.frm-grid').find('tr').length - 2; // the -2 comes from 1 for the header row and 1 for the fact that we're 0-based
+			var max_col=jQuery('#'+this.id).parents('tr').find('td input').length -1; // the -1 is for the fact that we're 0-based
+			switch (e.which){
+			case 37: // left arrow
+				if (col > 0){col--;}else if(row > 0){col=max_col;row--;}
+				break;
+			case 38: // up arrow
+				if(row > 0){row--;}else if(col > 0){col--;row=max_row;}
+				e.preventDefault(); // prevent list of previously entered values showing up and confusing
+				break;
+			case 39: // right arrow
+				if (col < max_col){col++;}else if (row < max_row){row++;col=0;}
+				break;
+			case 40: // down arrow
+				if (row < max_row){row++;}else if (col < max_col){col++;row=0;}
+				break;
+			}
+			if (row != parseInt(matches[1]) || col != parseInt(matches[2])){
+				// need to reset the focus
+				jQuery('#' + this.id.replace(pattern,'_'+row+'_'+col)).focus();
+			}
+		}
+	}
+}); */
+});
+function frm_remove_tag(html_tag){jQuery(html_tag).remove();}
+
+function frm_show_div(div,value,show_if,class_id){
+if(value==show_if) jQuery(class_id+div).fadeIn('slow'); else jQuery(class_id+div).fadeOut('slow');
+}
+function frm_select_item_checkbox(checked){if(!checked){jQuery(".select-all-item-action-checkboxes").removeAttr("checked");}}
+
+function frm_select_all_checkboxes(checked){
+if(checked){
+jQuery(".item-action-checkbox").attr("checked","checked");jQuery(".select-all-item-action-checkboxes").attr("checked","checked");
+}else{
+jQuery(".item-action-checkbox").removeAttr("checked");jQuery(".select-all-item-action-checkboxes").removeAttr("checked");
+}
+}
+
+function frmAddNewForm(form,action){
+	if(form !='') window.location='?page=formidable&action='+action+'&id='+form;
+}
+function frmRedirectToForm(form,action){
+	if(form !='') window.location='?page=formidable-entries&action='+action+'&form='+form;
+}
+
+function add_frm_field_link(form_id, field_type, ajax_url){
+jQuery.ajax({type:"POST",url:ajax_url,data:"action=frm_insert_field&form_id="+form_id+"&field="+field_type,
+success:function(msg){jQuery('#new_fields').append(msg);}
+});
+};
+
+function frm_duplicate_field(field_id,ajax_url){  
+jQuery.ajax({type:"POST",url:ajax_url,data:"action=frm_duplicate_field&field_id="+field_id,
+success:function(msg){jQuery('#new_fields').append(msg);}
+});
+};
+
+function frm_mark_required(field_id, required, images_url, ajax_url){
+    var thisid='req_field_'+field_id;
+    if(required=='0'){var switch_to='1';var atitle='Click to Mark as Not Required';var checked='checked="checked"';
+	jQuery('.frm_required_details'+field_id).fadeIn('slow');}
+	else{var switch_to='0';var atitle='Click to Mark as Required';var checked='';
+	jQuery('.frm_required_details'+field_id).fadeOut('slow');}
+    jQuery('#'+thisid).replaceWith('<a href="javascript:frm_mark_required('+field_id+','+switch_to+',\''+images_url+'\',\''+ajax_url+'\')" class="alignleft frm_required'+switch_to+'" id="'+thisid+'" title="'+atitle+'"><img src="'+images_url+'/required.png" alt="required"></a>');
+	jQuery('#frm_'+thisid).replaceWith('<input type="checkbox" id="frm_'+thisid+'" name="field_options[required_'+field_id+']" value="1" '+checked+' onclick="frm_mark_required('+field_id+','+switch_to+',\''+images_url+'\',\''+ajax_url+'\')" />');
+    jQuery.ajax({type:"POST",url:ajax_url,data:"action=frm_mark_required&field="+field_id+"&required="+switch_to});
+};
+
+function frm_clear_on_focus(field_id, active, images_url, ajax_url){
+    var thisid='clear_field_'+field_id;
+    if (active=='1'){var switch_to='0';var new_class='frm_inactive_icon';}
+    else{var switch_to='1';var new_class='';}
+    jQuery('#'+thisid).replaceWith('<a href="javascript:frm_clear_on_focus('+field_id+','+switch_to+',\''+images_url+'\',\''+ajax_url+'\')" class="'+new_class +' frm-show-hover" id="'+thisid+'"><img src="'+images_url+'/reload.png"></a>');
+    jQuery.ajax({type:"POST",url:ajax_url,data:"action=frm_clear_on_focus&field="+field_id+"&active="+switch_to});
+};
+
+function frm_default_blank(field_id, active, images_url, ajax_url){
+    var thisid='default_blank_'+field_id;
+    if(active=='1'){var switch_to='0';var new_class='frm_inactive_icon';}
+	else{var switch_to='1';var new_class='';}
+    jQuery('#'+thisid).replaceWith('<a href="javascript:frm_default_blank('+field_id+','+switch_to+',\''+images_url+'\',\''+ajax_url+'\')" class="'+new_class+' frm-show-hover" id="'+thisid+'"><img src="'+images_url+'/error.png"></a>');
+    jQuery.ajax({type:"POST",url:ajax_url,data:"action=frm_default_blank&field="+field_id+"&active="+switch_to});
+};
+
+function frm_add_field_option(field_id, ajax_url, table){
+	var data = {action:'frm_add_field_option',field_id:field_id,t:table};
+	jQuery.post(ajax_url,data,function(msg){
+		jQuery('#frm_add_field_'+field_id).before(msg);
+		if(table=='row'){ jQuery('#frm-grid-'+field_id+' tr:last').after(msg);}
+	});
+};
+
+function frm_delete_field_option(field_id, opt_key, ajax_url){
+    jQuery.ajax({type:"POST",url:ajax_url,
+        data:"action=frm_delete_field_option&field_id="+field_id+"&opt_key="+opt_key,
+        success:function(msg){ jQuery('#frm_delete_field_'+field_id+'-'+opt_key+'_container').fadeOut("slow");}
+    });
+};
+
+function frm_field_hover(show, field_id){
+	var html_id = '#frm_field_id_'+field_id;
+	if(show){jQuery(html_id).children(".frm-show-hover").show();}
+	else{if(!jQuery(html_id).is('.selected')){jQuery(html_id).children(".frm-show-hover").hide();}}
+}
+
+function frmSetMenuOffset() { 
+	var fields = jQuery('#frm_form_options .themeRoller');
+	if (!fields) return;
+	var currentOffset = document.documentElement.scrollTop || document.body.scrollTop; // body for Safari
+	var desiredOffset = 315 - currentOffset;
+	if (desiredOffset < 10) desiredOffset = 10;
+	//if (desiredOffset != parseInt(header.style.top)) 
+		fields.attr('style', 'top:'+desiredOffset + 'px;');
+}
+
+function frmDisplayFormSelected(form_id, ajax_url){
+    if (form_id == '') return;
+    jQuery.ajax({type:"POST",url:ajax_url,
+        data:"action=frm_get_field_tags&form_id="+form_id,
+        success:function(html){ jQuery('#content_fields').html(html);}
+    });
+    jQuery.ajax({type:"POST",url:ajax_url,
+        data:"action=frm_get_field_tags&target_id=dyncontent&form_id="+form_id,
+        success:function(html){ jQuery('#dyncontent_fields').html(html);}
+    });
+    jQuery.ajax({type:"POST",url:ajax_url,
+        data:"action=frm_get_entry_select&form_id="+form_id,
+        success:function(html){ jQuery('#entry_select_container').html(html);}
+    });
+};
+
+function frmInsertFieldCode(element_id, variable){
+	if(!element_id || element_id == 'content'){
+		send_to_editor(variable);
+		return;
+	}
+	var content_box=jQuery("#"+element_id);
+	if(content_box){
+		if(document.selection){content_box[0].focus();document.selection.createRange().text=variable;}
+		else if(content_box[0].selectionStart){obj=content_box[0];obj.value=obj.value.substr(0,obj.selectionStart)+variable+obj.value.substr(obj.selectionEnd,obj.value.length);}
+		else{content_box.val(variable+content_box.val());}
+	}
+}
+
+function frm_delete_row(field_id,opt_key,ajax_url){
+	if (jQuery('#frm-grid-'+field_id+' tr').length == 2){ // header row and only one data row
+		alert('Sorry, you must leave at least one row in this table.');
+	}else if(confirm('Are you sure you wish to permanently delete this row? This cannot be undone.')){
+		jQuery.ajax({type:"POST",url:ajax_url,
+	        data:"action=frm_delete_field_option&field_id="+field_id+"&opt_key="+opt_key,
+	        success:function(msg){jQuery('#frm-grid-'+field_id+' tr.'+opt_key).fadeOut("slow");frm_adjust_row_numbers(field_id);}
+	    });
+	}
+}
+
+function frm_delete_col(field_id,opt_key,ajax_url){
+	if (jQuery('#frm-grid-'+field_id+' th').length == 2){ // header col and only one data col
+		alert('Sorry, you must leave at least one column in this table.');
+	}else if(confirm('Are you sure you wish to permanently delete this column? This cannot be undone.')){
+		jQuery.ajax({type:"POST",url:ajax_url,
+			data:"action=frm_delete_field_option&field_id="+field_id+"&opt_key="+opt_key,
+		    success:function(msg){
+				jQuery('#frm-grid-'+field_id+' td.'+opt_key).fadeOut("slow");
+				jQuery('#frm-grid-'+field_id+' th.'+opt_key).fadeOut("slow");
+				frm_adjust_row_numbers(field_id);
+			}
+		});
+	}
+}
+
+function frm_adjust_row_numbers(field_id){
+	var row_num;
+	jQuery('#frm-grid-'+field_id+' tr').each(function(){
+		if (row_num == null){
+			// skip the first row (column headers)
+			row_num=0;
+		}else{
+			// This searches for inputs and readjusts their name to match the new row numbering scenario
+			jQuery(this).find('input').each(function(){  //input[name^=item_meta]
+				var name=jQuery(this).attr('name');
+				name=name.replace(/\[[0-9]+\]\[\]/,'['+row_num+'][]');
+				jQuery(this).attr('name',name);
+
+				var id=jQuery(this).attr('id');
+				id=id.replace(/_[0-9]+(_[0-9]+)$/,'_'+row_num+'$1');
+				jQuery(this).attr('id',id);
+			});
+			
+			// Now replace the javascript (for delete_row)
+			jQuery(this).find('a').each(function(){
+				var href=jQuery(this).attr('href');
+				href=href.replace(/(delete_row\([0-9]+,)[0-9]+/,'$1'+row_num);
+				jQuery(this).attr('href',href);
+			});
+			
+			// Finally, need to reset the class for the row
+			jQuery(this).get(0).className=jQuery(this).get(0).className.replace(/\brow-.*?\b/g,'');
+			jQuery(this).addClass("row-"+row_num);
+			row_num++;
+		}
+	});
+}
+
+var frm_active_requests=0; // = false;
+function frm_add_row(field_id,ajax_url){
+    jQuery.ajax({
+        type:"POST",url:ajax_url,
+        data:"action=frm_add_table_row&field_id="+field_id+"&row_num="+(jQuery('#frm-grid-'+field_id+' tr').length-1+frm_active_requests++),
+        success:function(msg){
+			frm_active_requests--;jQuery('#frm-grid-'+field_id+' tr:last').after(msg);
+			frm_post_add_row(field_id,jQuery('#frm-grid-'+field_id+' tr:last'));
+		}
+    });
+}
+
+function frm_post_add_row(field_id,new_row){
+	// Just a stub that can be overridden by another script
+}
+
+function frmSettingsTab(tab, id){
+	var t = tab.attr('href');
+	tab.parent().addClass('tabs').siblings('li').removeClass('tabs');
+	jQuery('#general_settings,#styling_settings').hide();
+	jQuery('#'+id+'_settings').show();
+	return false;
+}
