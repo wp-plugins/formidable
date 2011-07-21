731dcb2c31e472a5589aefb5489f30d2f94ede5d
sswells <sswells@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-21 16:20:03 +0000

    v1.5.5 release
    git-svn-id: https://plugins.svn.wordpress.org/formidable/tags/1.05.05@413350 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/views/frm-entries/errors.php
@@ -0,0 +1,36 @@
+<?php global $frm_settings; 
+if (isset($message) && $message != ''){ 
+    if(is_admin()){ 
+        ?><div id="message" class="updated fade" style="padding:5px;"><?php echo $message ?></div><?php 
+    }else{ 
+        echo $message; 
+    }
+} 
+
+if( isset($errors) && is_array($errors) && !empty($errors) ){
+    global $frm_settings;
+?>
+<div class="<?php echo (is_admin()) ? 'error' : 'frm_error_style' ?>"> 
+<?php 
+if(!is_admin()){ 
+    $img = apply_filters('frm_error_icon', FRM_IMAGES_URL . '/error.png');
+    if($img and !empty($img)){
+    ?><img src="<?php echo $img ?>" alt="" />
+<?php 
+    }
+} 
+    echo stripslashes($frm_settings->invalid_msg);
+    
+if(empty($frm_settings->invalid_msg)){
+    $show_img = false;
+    foreach( $errors as $error ){
+        if($show_img and $img and !empty($img)){ 
+            ?><img src="<?php echo $img ?>" alt="" /><?php 
+        }else{
+            $show_img = true;
+        }
+        echo stripslashes($error) . '<br/>';
+    }
+} ?>
+</div>
+<?php } ?>
