74a8b3b807eb6980e71e1589228e506450f477b6
srwells <srwells@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-08 21:36:34 +0000

    style subnav
    git-svn-id: https://plugins.svn.wordpress.org/formidable/trunk@460739 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/views/shared/form-nav.php
+++ classes/views/shared/form-nav.php
@@ -1,6 +1,7 @@
-<h2>
-    <a href="<?php echo admin_url('admin.php?page='.FRM_PLUGIN_NAME) ?>&amp;action=edit&amp;id=<?php echo $id ?>"><?php _e('Build', 'formidable') ?></a> >
-    <a href="<?php echo admin_url('admin.php?page='.FRM_PLUGIN_NAME) ?>&amp;action=settings&amp;id=<?php echo $id ?>"><?php _e('Settings', 'formidable') ?></a> >
-    <a href="<?php echo admin_url('admin.php?page='.FRM_PLUGIN_NAME) ?>-entries&amp;action=list&amp;form=<?php echo $id ?>&amp;show_nav=1"><?php _e('Entries', 'formidable') ?></a> >
-    <a href="<?php echo admin_url('admin.php?page='.FRM_PLUGIN_NAME) ?>-reports&amp;action=show&amp;form=<?php echo $id ?>&amp;show_nav=1"><?php _e('Reports', 'formidable') ?></a>
-</h2>
+<ul class="frm_form_nav">
+	 <li class="last"> <a class="current_page" href="<?php echo admin_url('admin.php?page='.FRM_PLUGIN_NAME) ?>-reports&amp;action=show&amp;form=<?php echo $id ?>&amp;show_nav=1"><?php _e('Reports', 'formidable') ?></a></li>
+	<li> <a href="<?php echo admin_url('admin.php?page='.FRM_PLUGIN_NAME) ?>-entries&amp;action=list&amp;form=<?php echo $id ?>&amp;show_nav=1"><?php _e('Entries', 'formidable') ?></a></li>
+    <li><a href="<?php echo admin_url('admin.php?page='.FRM_PLUGIN_NAME) ?>&amp;action=settings&amp;id=<?php echo $id ?>"><?php _e('Settings', 'formidable') ?></a> </li>
+<li class="first"><a href="<?php echo admin_url('admin.php?page='.FRM_PLUGIN_NAME) ?>&amp;action=edit&amp;id=<?php echo $id ?>"><?php _e('Build', 'formidable') ?></a> </li>
+  <div style="clear:both;"></div>
+</ul>
