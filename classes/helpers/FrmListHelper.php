246c59d9c635c7a723e975d33c3e19580de2510c
sswells <sswells@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-05 22:26:18 +0000

    2.0rc3 release
    git-svn-id: https://plugins.svn.wordpress.org/formidable/tags/2.0rc3@1106408 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/helpers/FrmListHelper.php
@@ -0,0 +1,29 @@
+<?php
+if ( !defined('ABSPATH') ) die('You are not allowed to call this page directly.');
+
+class FrmListHelper extends WP_List_Table {
+    var $params;
+
+	function __construct($args) {
+	    $args = wp_parse_args( $args, array(
+			'params' => array()
+		) );
+
+		$this->params = $args['params'];
+
+		parent::__construct( $args );
+	}
+
+	function ajax_user_can() {
+		return current_user_can( 'administrator' );
+	}
+
+	function display_rows() {
+		$style = '';
+		foreach ( $this->items as $item ) {
+			$style = ( ' class="alternate"' == $style ) ? '' : ' class="alternate"';
+			echo "\n\t", $this->single_row( $item, $style );
+		}
+	}
+
+}
